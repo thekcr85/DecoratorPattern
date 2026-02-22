8c3df63070e1b5db3dde2d4db9e43735ab87d239
thekcr85 <thekcr85@gmail.com>
2026-02-22 14:48:37 +0100

    Add EndpointExtensions for dynamic endpoint registration and mapping

---
+++ src/DecoratorPattern.API/Extensions/EndpointExtensions.cs
@@ -0,0 +1,31 @@
+﻿using DecoratorPattern.API.Endpoints;
+using System.Reflection;
+
+namespace DecoratorPattern.API.Extensions;
+
+public static class EndpointExtensions
+{
+	public static IServiceCollection AddEndpoints(this IServiceCollection services)
+	{
+		var endpointTypes = Assembly.GetExecutingAssembly()
+			.GetTypes()
+			.Where(t => typeof(IEndpoint).IsAssignableFrom(t) && t is { IsInterface: false, IsAbstract: false });
+
+		foreach (var type in endpointTypes)
+			services.AddScoped(typeof(IEndpoint), type);
+
+		return services;
+	}
+
+	public static IApplicationBuilder MapEndpoints(this WebApplication app)
+	{
+		var endpoints = app.Services
+			.CreateScope().ServiceProvider
+			.GetRequiredService<IEnumerable<IEndpoint>>();
+
+		foreach (var endpoint in endpoints)
+			endpoint.MapEndpoints(app);
+
+		return app;
+	}
+}
