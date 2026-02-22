05acf49509584841bbfe88ed3c8da5d39d067e6b
thekcr85 <thekcr85@gmail.com>
2026-02-22 13:44:20 +0100

    Add ApplicationServiceExtensions to register application services and decorators

---
+++ src/DecoratorPattern.Application/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using DecoratorPattern.Application.Decorators;
+using DecoratorPattern.Application.Interfaces;
+using DecoratorPattern.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DecoratorPattern.Application.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+	public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+	{
+		services.AddScoped<IUserService, UserService>();
+		services.Decorate<IUserService, LoggingUserServiceDecorator>();
+
+		return services;
+	}
+}
