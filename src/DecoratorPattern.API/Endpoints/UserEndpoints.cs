b0123b7bcb33d1de9c1c09791c00ab9bc8b4e441
thekcr85 <thekcr85@gmail.com>
2026-02-22 14:46:26 +0100

    Add UserEndpoints class for user-related endpoint mapping

---
+++ src/DecoratorPattern.API/Endpoints/UserEndpoints.cs
@@ -0,0 +1,18 @@
+﻿using DecoratorPattern.Application.Interfaces;
+
+namespace DecoratorPattern.API.Endpoints;
+
+public class UserEndpoints : IEndpoint
+{
+	public void MapEndpoints(IEndpointRouteBuilder app)
+	{
+		var group = app.MapGroup("/users");
+		group.MapGet("/{id:guid}", GetUserByIdAsync);
+	}
+
+	private static async Task<IResult> GetUserByIdAsync(Guid id, IUserService userService, CancellationToken cancellationToken)
+	{
+		var user = await userService.GetUserAsync(id, cancellationToken);
+		return user is not null ? Results.Ok(user) : Results.NotFound();
+	}
+}
