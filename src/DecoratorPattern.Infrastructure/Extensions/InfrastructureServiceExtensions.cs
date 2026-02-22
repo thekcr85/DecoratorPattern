d30092e0c1551bf388502cedf7f5645ead8da146
thekcr85 <thekcr85@gmail.com>
2026-02-22 13:48:53 +0100

    Add InfrastructurenServiceExtensions to register infrastructure services

---
+++ src/DecoratorPattern.Infrastructure/Extensions/InfrastructureServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using DecoratorPattern.Application.Interfaces;
+using DecoratorPattern.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DecoratorPattern.Application.Extensions;
+
+public static class InfrastructureServiceExtensions
+{
+	public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+	{
+		services.AddScoped<IUserRepository, UserRepository>();
+
+		return services;
+	}
+}
